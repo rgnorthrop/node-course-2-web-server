65d8af5314ef2ff4c98f0a1939668862d623ae60
rob <rgnorthrop@gmail.com>
2018-01-15 19:13:57 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to server.log.');
+        }
+    });
+    next();
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalizeIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (request, response) => {
+    // response.send('<h1>test response</h1>');
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/json', (request, response) => {
+    response.send({
+        prop1: 'property1',
+        prop2: 'property2'
+    })
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        error: 'error handling request'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+});
